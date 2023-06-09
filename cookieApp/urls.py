0154dd84624f1061b0f3f8a6d1deb85a3d523f2c
phani545 <phanideepchalla@gmail.com>
2023-06-09 15:46:02 +0530

    shoppingcart

--- cookieApp/urls.py
+++ cookieApp/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
     path('',views.home),
     path('page2/',views.page2),
     path('count/',views.countView),
+    path('index/',views.index),
+    path('addItem/',views.addItem),
+    path('displayItems/',views.displayCart),
 
 ]
--- cookieApp/views.py
+++ cookieApp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .forms import ItemForm
 
 # Create your views here.
 
@@ -24,3 +25,21 @@ def countView(request):
     response = render(request,'cookieApp/count.html',{'count':count})
     response.set_cookie('count',count)
     return response
+
+def index(request):
+    return render(request,'cookieApp/index.html')
+
+def addItem(request):
+    form=ItemForm()
+    response = render(request, 'cookieApp/addItem.html',{'form':form})
+    if request.method=='POST':
+        form=ItemForm(request.POST)
+        if form.is_valid():
+            name=form.cleaned_data['name']
+            quantity=form.cleaned_data['quantity']
+            response.set_cookie(name,quantity,120)
+    return response
+
+
+def displayCart(request):
+    return render(request,'cookieApp/displayItems.html')
