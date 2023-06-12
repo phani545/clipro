bf0fca4b182854c608a40fe84ba1a7ae61d6dd1a
phani545 <phanideepchalla@gmail.com>
2023-06-12 23:28:36 +0530

    sessionAPI

--- clipro/settings.py
+++ clipro/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'CrudFunc',
     'classbasedview',
     'cookieApp',
+    'sessionAPI',
 ]
 
 MIDDLEWARE = [
---
+++ sessionAPI/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SessionapiConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'sessionAPI'
